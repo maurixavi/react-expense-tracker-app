ad35e37d24ab4beacd6f02ba090de3a15bb3135c
maurixavi <mxavierarevalo@gmail.com>
2024-03-24 03:26:27 -0300

    Add type of transaction field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
@@ -12,7 +11,6 @@ import './App.css';
 function App() {
   return (
     <GlobalProvider>
-      <Header />
       <div className='container'>
         <Balance />
         <IncomeExpenses />
--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -4,6 +4,7 @@ import { GlobalContext } from '../context/GlobalState';
 export const AddTransaction = () => {
 	const [text, setText] = useState('');
 	const [amount, setAmount] = useState(0);
+	const [type, setType] = useState('Expense');
 
 	const { addTransaction } = useContext(GlobalContext);
 	
@@ -12,27 +13,38 @@ export const AddTransaction = () => {
 		
 		const newTransaction = {
 			id: Math.floor(Math.random() * 100000000),
+			type,
 			text,
-			amount: +amount
+			amount: type === "Income" ? +amount : -amount
+			//amount: +amount
 		}
 
 		addTransaction(newTransaction);
+
+		setText('');
+  	setAmount(0);
 	}
 
   return (
 	<>
-		<h3>Add new transaction</h3>
+		<h3>Add Transaction</h3>
 		<form onSubmit={onSubmit}>
 			<div className="form-control">
-				<label htmlFor="text">Text</label>
-				<input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..."></input>
+				<select value={type} onChange={(e) => setType(e.target.value)}>
+					<option value="Expense">Expense</option>
+					<option value="Income">Income</option>
+				</select>
 			</div>
 			<div className="form-control">
-				<label htmlFor="amount">Amount <br /> (negative - expense, positive - income
-				</label>
-				<input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..."></input>
+				<p>Amount</p>
+				<label className="amount-prefix">$</label><input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="" ></input>
 			</div>
-			<button className="btn">Add transaction</button>
+
+			<div className="form-control">
+				<input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Description"></input>
+			</div>
+			
+			<button className="btn">Add {type}</button>
 		</form>
 
 	</>
