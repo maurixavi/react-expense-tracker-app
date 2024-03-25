518f4d3b233d8b7216477245785d3a4cb0fcdda3
maurixavi <mxavierarevalo@gmail.com>
2024-03-25 02:07:36 -0300

    Add filter transactions All-Recent

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,19 +1,32 @@
-import React, { useContext } from 'react'
-
+import React, { useContext, useState } from 'react';
 import { Transaction } from './Transaction';
-
-import { GlobalContext } from '../context/GlobalState'
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
-	
-	return (
-	<>
-		<h3>Transactions</h3>
-		<ul className="list">
-			{transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
-		</ul>
+  const [showAll, setShowAll] = useState(false);
 
-	</>
-  )
-}
+  // Sort transactions by date in descending order
+  const sortedTransactions = transactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+  const displayedTransactions = showAll ? sortedTransactions : sortedTransactions.slice(0, 5);
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+  };
+
+  return (
+    <>
+		<h3>Transactions</h3>
+      <div className="transactions-header">
+        <button className="filter-transactions-btn" onClick={toggleShowAll}>
+          {showAll ? 'View Recent' : 'View All'}
+        </button>
+      </div>
+      <ul className="list">
+        {displayedTransactions.map(transaction => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </>
+  );
+};
